921e889066631cd555e596b1d6af6ea3ee8b4b19
Vladimir Pavlov <vovavc@gmail.com>
2023-05-27 00:53:06 +0200

    inline if for verbose variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,12 @@
 
 sh -c "
     echo Envs are:
-    verbose=''
-    if [ ! -z '$INPUT_VERBOSE' -a '$INPUT_VERBOSE' != '' ]; then
-        verbose='verbose'
-    fi
+    VERBOSE=$([! -z $INPUT_VERBOSE -a $INPUT_VERBOSE != '' ] && '-verbose' || '')
     echo $INPUT_PATH
     echo $INPUT_DATABASE
     echo $INPUT_VERBOSE
     echo $INPUT_COMMAND
     echo Envs end;
-    echo command is: migrate -path $INPUT_PATH -database $INPUT_DATABASE $verbose $INPUT_COMMAND
-    migrate -path $INPUT_PATH -database $INPUT_DATABASE $verbose $INPUT_COMMAND
+    echo command is: migrate -path $INPUT_PATH -database $INPUT_DATABASE $VERBOSE $INPUT_COMMAND
+    migrate -path $INPUT_PATH -database $INPUT_DATABASE $VERBOSE $INPUT_COMMAND
 "
