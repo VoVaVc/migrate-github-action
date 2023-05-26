91d6eb41f026bd3b507b52f8556482bad1505b2c
Vladimir Pavlov <vovavc@gmail.com>
2023-05-27 01:20:02 +0200

    fix if/else verbose statement semantics

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-sh -c "
+sh -c '
     echo Envs are:
-    VERBOSE=$([ -z $INPUT_VERBOSE ] && echo "" || echo "-verbose");
+    if [ -z $INPUT_VERBOSE ] 
+    then
+        VERBOSE="fuck"
+    else
+        VERBOSE="-verbose"
+    fi
     echo $INPUT_PATH
     echo $INPUT_DATABASE
     echo $INPUT_VERBOSE
+    echo verbose flag will be $VERBOSE;
     echo $INPUT_COMMAND
     echo Envs end;
     echo command is: migrate -path $INPUT_PATH -database $INPUT_DATABASE $VERBOSE $INPUT_COMMAND
     migrate -path $INPUT_PATH -database $INPUT_DATABASE $VERBOSE $INPUT_COMMAND
-"
+'
