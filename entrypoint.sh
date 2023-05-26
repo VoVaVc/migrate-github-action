e97a6e05126d4db11b3cd46912a522cd7abb7984
Vladimir Pavlov <vovavc@gmail.com>
2023-05-27 00:46:45 +0200

    render verbose argument only if verbose not null

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@ set -eu
 
 sh -c "
     echo Envs are:
+    verbose=''
+    if [ ! -z '$INPUT_VERBOSE' -a '$INPUT_VERBOSE' != '' ]; then
+        verbose='verbose'
+    fi
     echo $INPUT_PATH
     echo $INPUT_DATABASE
     echo $INPUT_VERBOSE
     echo $INPUT_COMMAND
     echo Envs end;
-    echo command is: migrate -path $INPUT_PATH -database $INPUT_DATABASE -verbose $INPUT_VERBOSE $INPUT_COMMAND
-    migrate -path $INPUT_PATH -database $INPUT_DATABASE -verbose $INPUT_VERBOSE $INPUT_COMMAND 2
+    echo command is: migrate -path $INPUT_PATH -database $INPUT_DATABASE $verbose $INPUT_COMMAND
+    migrate -path $INPUT_PATH -database $INPUT_DATABASE $verbose $INPUT_COMMAND
 "
